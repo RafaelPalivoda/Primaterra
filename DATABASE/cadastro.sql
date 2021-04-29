56f21d7bf0f5c9be87fe7765e58e37c868b293fe
RafaelPalivodaAb <dev01@absolucoesemti.com.br>
2021-04-29 12:59:31 -0300

    update Banco de Dados

--- DATABASE/cadastro.sql
+++ DATABASE/cadastro.sql
@@ -1,16 +1,16 @@
 CREATE DATABASE Sistema;
 CREATE TABLE IF NOT EXISTS Entidade(
-    idEntidade SERIAL primary key NOT NULL ,
+    idEntidade SERIAL PRIMARY KEY NOT NULL ,
     codigoEntidade int,
     nome varchar(200),
     razaoSocial varchar(300),
     cnpjCpf int,
     rg int,
     dtFundacao int,
-    Usuario int,
-    ProprietarioArea int,
-    Incorporadora int,
-    PrestadorServiço int,
+    isUsuario int,
+    isProprietarioArea int,
+    isIncorporadora int,
+    isPrestadorServiço int,
     sexo char,
     cargo varchar(50),
     dtCadastro date,
@@ -28,22 +28,22 @@ CREATE TABLE IF NOT EXISTS Entidade(
     FOREIGN KEY (idUsuario) REFERENCES Usuario (idUsuario)
 );
 CREATE TABLE IF NOT EXISTS Telefone(
-    codigoTelefone SERIAL primary key NOT NULL ,
+    codigoTelefone SERIAL PRIMARY KEY NOT NULL ,
     ddd int,
     fixo int,
     celular int
 );
 CREATE TABLE IF NOT EXISTS enquadFiscal(
-    codigoEnquad SERIAL primary key NOT NULL ,
+    codigoEnquad SERIAL PRIMARY KEY NOT NULL ,
     nomeEnquad varchar(200)
 );
 CREATE TABLE IF NOT EXISTS email(
-    codigoEmail SERIAL primary key NOT NULL ,
+    codigoEmail SERIAL PRIMARY KEY NOT NULL ,
     emailPrincipal varchar(300),
     emailSecundario varchar(300)
 );
 CREATE TABLE IF NOT EXISTS Contato(
-    codigoContato SERIAL primary key NOT NULL ,
+    codigoContato SERIAL PRIMARY KEY NOT NULL ,
     codigoemail int,
     codigotelefone int,
     codigoemail int,
@@ -52,11 +52,11 @@ CREATE TABLE IF NOT EXISTS Contato(
     FOREIGN KEY (codigotelefone) REFERENCES telefone (codigotelefone)
 );
 CREATE TABLE IF NOT EXISTS TipoContribuinte(
-    codigoContribuinte SERIAL primary key NOT NULL ,
+    codigoContribuinte SERIAL PRIMARY KEY NOT NULL ,
     tipoContribuinte varchar(100)
 );
 CREATE TABLE IF NOT EXISTS Endereco(
-    codigoEndereco SERIAL primary key NOT NULL ,
+    codigoEndereco SERIAL PRIMARY KEY NOT NULL ,
     cep int,
     logradouro varchar(200),
     numero int,
@@ -69,29 +69,29 @@ CREATE TABLE IF NOT EXISTS Endereco(
     FOREIGN KEY (codigoTipoEndereco) REFERENCES TipoEndereco (codigoTipoEndereco)
 );
 CREATE TABLE IF NOT EXISTS TipoEndereco(
-    codigoTipoEndereco SERIAL primary key NOT NULL ,
+    codigoTipoEndereco SERIAL PRIMARY KEY NOT NULL ,
     TipoEndereco varchar(100) 
 );
 CREATE TABLE IF NOT EXISTS InscricaoEstadual(
-    codigoIE SERIAL primary key NOT NULL ,
+    codigoIE SERIAL PRIMARY KEY NOT NULL ,
     Inscricao int,
     codigoIes int,
     FOREIGN KEY (codigoIeS) REFERENCES InscricaoSubstituta (codigoIeS)
 );
 CREATE TABLE IF NOT EXISTS InscricaoSubstituta(
-    codigoIeS SERIAL primary key NOT NULL ,
+    codigoIeS SERIAL PRIMARY KEY NOT NULL ,
     InscricaoSubstituta int
 );
 CREATE TABLE IF NOT EXISTS Usuario(
-    IdUsuario SERIAL primary key NOT NULL ,
-    usuario varchar(50) NOT NULL UNIQUE,
-    senha varchar(50)
+    IdUsuario SERIAL PRIMARY KEY NOT NULL ,
+    usuario NOT NULL UNIQUE varchar(50),
+    senha varchar(50),
 );
 CREATE TABLE IF NOT EXISTS TipoProjeto(
-    IdTipoProjeto SERIAL primary key NOT NULL ,
+    IdTipoProjeto SERIAL PRIMARY KEY NOT NULL ,
     TipoProjeto varchar(100)
 );
 CREATE TABLE IF NOT EXISTS TipoNegociacao(
-    IdTipoNegociacao SERIAL primary key NOT NULL ,
+    IdTipoNegociacao SERIAL PRIMARY KEY NOT NULL ,
     TipoNegociacao varchar(50)
 );
