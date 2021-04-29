7437738cd8b1074548d38efde332a509b94a34da
RafaelPalivodaAb <dev01@absolucoesemti.com.br>
2021-04-29 13:29:17 -0300

    update

--- TopSecret/src/main/java/sistema/Cadastro.java
+++ TopSecret/src/main/java/sistema/Cadastro.java
@@ -4,9 +4,7 @@
 * Qualquer cópia não autorizada estará sujeita a sanções cíveis e criminais
 */
 package sistema;
-
 import java.util.Date;
-
 /*
 * Autores: Rafael Palivoda & Matheus Balão
 * Coordenação de: Alexsandro Barboza
@@ -15,110 +13,126 @@ public class Cadastro {
    public class Entidade{
        //Atributos Entidade
         private String nome, email, bairro, logradouro, cidade, estado, pais;
-        private Integer cep, numeroImovel, fixo, celular, telefone;
+        private Integer cep, numeroImovel, fixo, celular;
         private Date dtCadastro;
        //Métodos Getter e Setter...
         public String getNome() {
+            //getNome -> SELECT FROM Entidade WHERE nome = this.nome;
             return nome;
         }
 
         public void setNome(String nome) {
+            //INSERT INTO Entidade (nome);
             this.nome = nome;
         }
 
         public String getEmail() {
+            //getEmail -> SELECT FROM email WHERE emailPrincipal = this.email;
             return email;
         }
 
         public void setEmail(String email) {
+            //INSERT INTO email (email);
             this.email = email;
         }
 
         public String getBairro() {
+            //getBairro -> SELECT FROM Endereco WHERE bairro = this.bairro;
             return bairro;
         }
 
         public void setBairro(String bairro) {
+            //INSERT INTO Endereco (bairro);
             this.bairro = bairro;
         }
 
         public String getLogradouro() {
+            //getLogradouro -> SELECT FROM Endereco WHERE logradouro = this.logradouro;
             return logradouro;
         }
 
         public void setLogradouro(String logradouro) {
+            //INSERT INTO Endereco (logradouro);
             this.logradouro = logradouro;
         }
 
         public String getCidade() {
+            //getCidade -> SELECT FROM Endereco WHERE cidade = this.cidade;
             return cidade;
         }
 
         public void setCidade(String cidade) {
+            //INSERT INTO Endereco (cidade);
             this.cidade = cidade;
         }
 
         public String getEstado() {
+            //getEstado -> SELECT FROM Endereco WHERE estado = this.estado;
             return estado;
         }
 
         public void setEstado(String estado) {
+            //INSERT INTO Endereco (estado);
             this.estado = estado;
         }
 
         public String getPais() {
+            //getPais -> SELECT FROM Endereco WHERE pais = this.pais;
             return pais;
         }
 
         public void setPais(String pais) {
+            //INSERT INTO Endereco (pais);
             this.pais = pais;
         }
 
         public Integer getCep() {
+            //getCep -> SELECT FROM Endereco WHERE cep = this.cep;
             return cep;
         }
 
         public void setCep(Integer cep) {
+            //INSERT INTO Endereco (cep);
             this.cep = cep;
         }
 
         public Integer getNumeroImovel() {
+            //getNumeroImovel -> SELECT FROM Endereco WHERE numero = this.numeroImovel;
             return numeroImovel;
         }
 
         public void setNumeroImovel(Integer numeroImovel) {
+            //INSERT INTO Endereco (numero);
             this.numeroImovel = numeroImovel;
         }
 
         public Integer getFixo() {
+            //getFixo -> SELECT FROM Telefone WHERE fixo = this.fixo;
             return fixo;
         }
 
         public void setFixo(Integer fixo) {
+            //INSERT INTO Telefone (fixo);
             this.fixo = fixo;
         }
 
         public Integer getCelular() {
+            //getCelular -> SELECT FROM Telefone WHERE celular = this.celular;
             return celular;
         }
 
         public void setCelular(Integer celular) {
+            //INSERT INTO Telefone (celular);
             this.celular = celular;
         }
 
-        public Integer getTelefone() {
-            return telefone;
-        }
-
-        public void setTelefone(Integer telefone) {
-            this.telefone = telefone;
-        }
-
         public Date getDtCadastro() {
+            //getDtCadastro -> SELECT FROM Entidade WHERE dtCadastro = this.dtCadastro;
             return dtCadastro;
         }
 
         public void setDtCadastro(Date dtCadastro) {
+            //INSERT INTO Entidade (dtCadastro);
             this.dtCadastro = dtCadastro;
         }
         //----------------------------------//
@@ -130,55 +144,64 @@ public class Cadastro {
             //Métodos Getter e Setter..
 
            public String getRazaosocial() {
+               //getRazasocial -> SELECT FROM Entidade WHERE razaoSocial = this.razaosocial;
                return razaosocial;
            }
 
            public void setRazaosocial(String razaosocial) {
+               //INSERT INTO Entidade (razaoSocial);
                this.razaosocial = razaosocial;
            }
 
            public Integer getCnpj() {
+               //getCnpj -> SELECT FROM Entidade WHERE cnpjCpf = this.cnpj;
                return cnpj;
            }
 
            public void setCnpj(Integer cnpj) {
+               //INSERT INTO Entidade (cpfCnpj);
                this.cnpj = cnpj;
            }
 
            public Integer getEnquadFiscal() {
+               //getEnquadFiscal -> SELECT FROM Entidade WHERE enquadFiscal = this.enquadFiscal;
                return enquadFiscal;
            }
 
            public void setEnquadFiscal(Integer enquadFiscal) {
+               //INSERT INTO Entidade (enquadFiscal);
                this.enquadFiscal = enquadFiscal;
            }
 
            public Integer getTipoContribuinte() {
+               //getTipoContribuinte -> SELECT FROM Entidade WHERE tipoContribuinte = this.tipoContribuinte;
                return tipoContribuinte;
            }
 
            public void setTipoContribuinte(Integer tipoContribuinte) {
+               //INSERT INTO Entidade (tipoContribuinte);
                this.tipoContribuinte = tipoContribuinte;
            }
 
            public Date getDtFundacao() {
+               //getDtFundacao -> SELECT FROM Entidade WHERE dtFundacao = this.dtFundacao;
                return dtFundacao;
            }
 
            public void setDtFundacao(Date dtFundacao) {
+               //INSERT INTO Entidade (dtFundacao);
                this.dtFundacao = dtFundacao;
            }
            //----------------------------------//
            //Método Construtor Pessoa Juridica
            public PessoaJuridica(String nome, String razaosocial, Integer cnpj, String email,
-                              Integer telefone, Integer cep, String bairro, String logradouro,
+                              Integer cep, String bairro, String logradouro,
                               Integer numeroImovel, String cidade, String estado, String pais,
                               Date dtFundacao, Integer enquadFiscal, Integer tipoContribuinte) {
             setNome(nome);
             setRazaosocial(razaosocial);
             setCnpj(cnpj);
             setEmail(email);
-            setTelefone(telefone);
             setCep(cep);
             setBairro(bairro);
             setLogradouro(logradouro);
@@ -197,33 +220,39 @@ public class Cadastro {
             private Boolean isUsuario;
             //Métodos Getter e Setter...
            public Integer getCpf() {
+               //getCpf -> SELECT FROM Entidade WHERE cpfCnpj = this.cpf;
                return cpf;
            }
 
            public void setCpf(Integer cpf) {
+               //INSERT INTO Entidade (cpfCnpj);
                this.cpf = cpf;
            }
 
            public Integer getRg() {
+               //getRg -> SELECT FROM Entidade WHERE rg = this.rg;
                return rg;
            }
 
            public void setRg(Integer rg) {
+               //INSERT INTO Entidade (rg);
                this.rg = rg;
            }
 
            public Boolean getIsUsuario() {
+               //getIsUsuario -> SELECT FROM Entidade WHERE isUsuario = this.isUsuario;
                return isUsuario;
            }
 
            public void setIsUsuario(Boolean isUsuario) {
+               //INSERT INTO Entidade (isUsuario);
                this.isUsuario = isUsuario;
            }
             //---------------------------------//
            // Método Construtor da Pessoa Fisica
         public PessoaFisica(String nome, Integer cpf, Integer rg, String email, Integer cep,
                             String bairro, String logradouro, String cidade, String estado, 
-                            String pais, Integer numeroImovel, Integer telefone) {
+                            String pais, Integer numeroImovel) {
         
             setNome(nome);
             setCpf(cpf);
@@ -236,7 +265,6 @@ public class Cadastro {
             setEstado(estado);
             setPais(pais);
             setNumeroImovel(numeroImovel);
-            setTelefone(telefone);
         }
       }
    }
