51e75940f71584665fbc9b85f5a9bfb31c9b5e6e
dev02AB <dev02@absolucoesemti.com.br>
2021-05-03 13:49:53 -0300

    v 0.1

--- ProjetoAB/README.md
+++ ProjetoAB/README.md
@@ -1,4 +1,4 @@
-# SistemaAB
+# ProjetoAB
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.10.
 
--- ProjetoAB/karma.conf.js
+++ ProjetoAB/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/SistemaAB'),
+      dir: require('path').join(__dirname, './coverage/ProjetoAB'),
       subdir: '.',
       reporters: [
         { type: 'html' },
