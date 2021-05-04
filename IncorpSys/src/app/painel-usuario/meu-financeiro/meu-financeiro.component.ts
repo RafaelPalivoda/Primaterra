d9f43d48657f7f4e0e0d80925012138e109d49e1
LeonardiBalão <79735743+LeonardiBalao@users.noreply.github.com>
2021-05-04 19:31:37 -0300

    a

---
+++ IncorpSys/src/app/painel-usuario/meu-financeiro/meu-financeiro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'meu-financeiro',
+  templateUrl: './meu-financeiro.component.html',
+  styleUrls: ['./meu-financeiro.component.scss']
+})
+export class MeuFinanceiroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
