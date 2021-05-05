39860c84884aa2b7c040d5581632582006acc698
LeonardiBalão <79735743+LeonardiBalao@users.noreply.github.com>
2021-05-04 21:16:20 -0300

    v.04

--- IncorpSystem/src/app/app.module.ts
+++ IncorpSystem/src/app/app.module.ts
@@ -5,10 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PaginaLoginComponent } from './pagina-login/pagina-login/pagina-login.component';
+import { PainelUsuarioComponent } from './painel-usuario/painel-usuario/painel-usuario.component';
+import { CadastrarEntidadeComponent } from './painel-usuario/cadastrar/cadastrar-entidade/cadastrar-entidade.component';
+import { PlanoDeContasComponent } from './painel-usuario/financeiro/plano-de-contas/plano-de-contas.component';
+import { MinhasAreasComponent } from './painel-usuario/areas/minhas-areas/minhas-areas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaLoginComponent,
+    PainelUsuarioComponent,
+    CadastrarEntidadeComponent,
+    PlanoDeContasComponent,
+    MinhasAreasComponent,
   ],
   imports: [
     BrowserModule,
---
+++ IncorpSystem/src/app/painel-usuario/financeiro/plano-de-contas/plano-de-contas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plano-de-contas',
+  templateUrl: './plano-de-contas.component.html',
+  styleUrls: ['./plano-de-contas.component.scss']
+})
+export class PlanoDeContasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
