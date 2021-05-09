a0c5566737532f384b8c6c4e9f6eb04a08aeb9ff
RafaelPalivodaAb <dev01@absolucoesemti.com.br>
2021-05-09 00:51:33 -0300

    update

--- Sistema/src/main/java/com/absolucoesemti/mgtIncorporadora/MgtIncorporadoraApplication.java
+++ Sistema/src/main/java/com/absolucoesemti/mgtIncorporadora/MgtIncorporadoraApplication.java
@@ -12,6 +12,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+@Service
 @Controller
 @SpringBootApplication
 public class MgtIncorporadoraApplication implements CommandLineRunner{
